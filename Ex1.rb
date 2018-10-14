d459a917fbb41fb9e0c1fcbecc7d380d2b95e290
j1nma <j1nma@users.noreply.github.com>
2018-10-14 19:27:17 -0300

    Add Ex2.rb

--- Ex1.rb
+++ Ex1.rb
@@ -14,10 +14,7 @@ data = Hash.new
   entries = Bio::GenBank.open(ARGV[0])
   entries.each_entry do |entry|
     seq = entry.to_biosequence
-    # puts "_____#{frame}_____"
-    # puts seq.translate(frame)
-    # puts '__________'
-    data[frame] = seq.translate(frame)
+    data[frame] = seq.translate(frame) unless seq.empty?
   end
 end
 
@@ -25,7 +22,7 @@ puts data
 
 
 (1..6).each do |frame|
-  File.open("ej1.out#{frame}.fas", 'w') do |f|
+  File.open("out/ej1.out#{frame}.fas", 'w+') do |f|
     seq = Bio::Sequence::NA.new(data[frame])
     f.write(seq.to_fasta) unless seq.empty?
   end
---
+++ Ex2.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+require 'bio'
+
+blast = Bio::Blast.remote('blastp', 'swissprot', '-e 0.0001', 'genomenet')
+
+(1..6).each do |frame|
+  ff = Bio::FlatFile.open(Bio::FastaFormat, "out/ej1.out#{frame}.fas")
+  File.open("out/blast#{frame}.out", 'w') do |f|
+    ff.each_entry do |entry|
+      report = blast.query(entry.seq)
+      report.each do |hit|
+        f.puts('___________________________')
+        f.puts "Bit score: #{hit.bit_score}"
+        f.puts "Identities: #{hit.identity}"
+        f.puts "Overlap: #{hit.overlap}"
+        f.puts "Query start: #{hit.query_start} end: #{hit.query_end}"
+        f.puts 'Target ID: ' + hit.target_id
+        f.puts 'Target def: ' + hit.target_def
+        f.puts "Target start: #{hit.target_start} end: #{hit.target_end}"
+        f.puts 'Sequence length: ' + hit.target_len.to_s
+        f.puts 'Sequence: ' + hit.target_seq
+      end
+    end
+  end
+end
